56686e0ae3948ca0b741be1115d15054299f389a
keyraphi <keyraphi@gmail.com>
2022-01-25 16:05:06 +0100

    Reordered requires such that packer is loaded

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
+require("packer-config")
 require("settings")
 require("mappings")
-require("packer-config")
 require("colorschemes-config.nightfox")
 require("nvim-tree-config")
 require("lsp-config.language-servers")
