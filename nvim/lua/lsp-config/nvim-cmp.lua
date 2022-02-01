62d806df0a9dcca165e93f5a1aeeee9cbd3de9be
keyraphi <keyraphi@gmail.com>
2022-02-01 11:01:09 +0100

    Fixed snippets

--- nvim/lua/lsp-config/nvim-cmp.lua
+++ nvim/lua/lsp-config/nvim-cmp.lua
@@ -2,6 +2,7 @@
 local lspkind = require("lspkind")
 
 -- luasnip helpers
+require("luasnip.loaders.from_vscode").load()
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -14,7 +15,7 @@ local cmp = require("cmp")
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
+            require('luasnip').lsp_expand(args.body)
 		end,
 	},
 	mapping = {
@@ -51,9 +52,9 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
-		{ name = "cmp_tabnine" },
+		{ name = "luasnip" },
 		{ name = "path" },
-		{ name = "vsnip" },
+		{ name = "cmp_tabnine" },
 		{ name = "buffer" },
 		-- { name = "cmdline" },
 	},
