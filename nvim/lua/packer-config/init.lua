b2eee5e17a98d94b9255d1869fff71be66f9b362
Raphael <keyraphi@gmail.com>
2022-05-03 16:44:33 +0200

    Comments for some plugins

--- nvim/lua/packer-config/init.lua
+++ nvim/lua/packer-config/init.lua
@@ -17,18 +17,18 @@ return require("packer").startup(function()
 	use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" }) -- tabnine
 	use("onsails/lspkind-nvim") -- pictograms for completion plugin
 	use("rcarriga/nvim-notify") -- notify plugin
-	use("kyazdani42/nvim-web-devicons")
-	use("nvim-lualine/lualine.nvim")
+	use("kyazdani42/nvim-web-devicons") -- icons
+	use("nvim-lualine/lualine.nvim")  -- the botom status line
 	use("romgrk/barbar.nvim") -- buffers as "tabs"
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) -- treesitter
 	use("nvim-treesitter/nvim-treesitter-refactor")
 	use("nvim-treesitter/nvim-treesitter-textobjects")
-	use("andymass/vim-matchup")
-	use("mfussenegger/nvim-ts-hint-textobject")
-	use("lewis6991/spellsitter.nvim")
+    use("andymass/vim-matchup")  -- use % to jump to language-specific pairs (g%, [%, ]%, z%)
+	use("mfussenegger/nvim-ts-hint-textobject")  -- highlights pairs (if, else) (whiel end) ...
+	use("lewis6991/spellsitter.nvim")  -- Spell checking in comments, not in code
 	use("SmiteshP/nvim-gps") -- context in status-line
 	use({
-		"nvim-telescope/telescope.nvim",
+		"nvim-telescope/telescope.nvim", -- telescope
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 	use("williamboman/nvim-lsp-installer")  -- install lsps via :LspInstallInfo
@@ -43,8 +43,8 @@ return require("packer").startup(function()
 	use("tpope/vim-repeat") -- make things like surround repeatable via "."
 	use("tommcdo/vim-lion") -- alignment via 'gl<motion><character>'
 	use("jedi2610/nvim-rooter.lua")  -- automatically set root directory
-	use("tpope/vim-commentary")
-	use("JoosepAlviste/nvim-ts-context-commentstring") -- comment stuff out via gc
+	use("tpope/vim-commentary")  -- comment stuff out using gc
+	use("JoosepAlviste/nvim-ts-context-commentstring") -- auto-detect what kind of commend is used by the language
 	-- use("ray-x/lsp_signature.nvim")  -- show function signature in completion window
     use("ggandor/lightspeed.nvim") -- fast navigation with s or S
     use("lervag/vimtex") -- latex stuff
