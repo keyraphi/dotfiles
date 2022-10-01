5844b372113b1b00018f8f0978abd4978fd507ad
Raphael <keyraphi@gmail.com>
2022-10-01 17:46:36 +0200

    Formatting stuff

--- nvim/lua/lsp-config/language-servers.lua
+++ nvim/lua/lsp-config/language-servers.lua
@@ -1,9 +1,8 @@
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.format{ async = True} <CR>", opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -13,22 +12,23 @@ local on_attach = function(client, bufnr)
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
     vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{ async = true }' ]])
-    -- require("lsp_signature").on_attach({  -- signature hints
-    -- 	bind = true, -- This is mandatory, otherwise border config won't get registered.
-    -- 	handler_opts = {
-    -- 		border = "rounded",
-    -- 	},
-    -- }, bufnr)
 end
 
 require("mason").setup()
@@ -37,9 +37,11 @@ require("mason-lspconfig").setup_handlers({
     -- The first entry (without a key) will be the default handler
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
-    end
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup({
+            on_attach = on_attach,
+        })
+    end,
 })
 
 -- settings for diagnostics
--- nvim/lua/lsp-config/null-ls.lua
+++ nvim/lua/lsp-config/null-ls.lua
@@ -1,28 +1,55 @@
 local null_ls = require("null-ls")
 
 local sources = {
-	null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-	null_ls.builtins.formatting.elm_format,
-	-- null_ls.builtins.formatting.clang_format,
-	null_ls.builtins.formatting.cmake_format,
-	null_ls.builtins.formatting.codespell,
-	null_ls.builtins.formatting.djhtml,
-	null_ls.builtins.formatting.gofmt,
-	null_ls.builtins.formatting.isort,
-	null_ls.builtins.formatting.json_tool,
-	null_ls.builtins.formatting.stylua,
-	null_ls.builtins.formatting.latexindent,
-	null_ls.builtins.formatting.markdownlint,
-	null_ls.builtins.formatting.pg_format,
-	null_ls.builtins.formatting.prettierd,
-	null_ls.builtins.formatting.shfmt,
-	null_ls.builtins.diagnostics.write_good,
-	null_ls.builtins.diagnostics.gitlint,
-	null_ls.builtins.diagnostics.pylint.with({
-		extra_args = { "--generated-members=numpy.* ,torch.*,zmq.PUSH,zmq.PULL,zmq.NOBLOCK" },
-	}),
+    null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+    null_ls.builtins.formatting.elm_format,
+    -- null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.formatting.cmake_format,
+    null_ls.builtins.formatting.codespell,
+    null_ls.builtins.formatting.djhtml,
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.json_tool,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.latexindent,
+    null_ls.builtins.formatting.markdownlint,
+    null_ls.builtins.formatting.pg_format,
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.diagnostics.write_good,
+    null_ls.builtins.diagnostics.gitlint,
+    null_ls.builtins.diagnostics.pylint.with({
+        extra_args = { "--generated-members=numpy.* ,torch.*,zmq.PUSH,zmq.PULL,zmq.NOBLOCK" },
+    }),
 }
 
 null_ls.setup({
-	sources = sources,
+    sources = sources,
 })
+
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+    })
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- add to your shared on_attach callback
+local on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                lsp_formatting(bufnr)
+            end,
+        })
+    end
+end
